b9ef892f1c71d0cf05b42aa7e970a5a61f4cd058
Jan Sarenik <jajomojo@gmail.com>
2022-06-03 12:12:27 +0200

    chroot.sh: Quote variables
    Fixes #3
    
        $ shellcheck chroot.sh
        $ echo $?
        $ 0

--- chroot.sh
+++ chroot.sh
@@ -3,7 +3,7 @@
 MYUSER=${SUDO_USER:-$USER}
 MACJAIL=${1:-"$PWD/mac-jail"}
 MACJAIL=${MACJAIL%/}
-test -d $MACJAIL || { echo "Directory $MACJAIL not found!"; exit 1; }
+test -d "$MACJAIL" || { echo "Directory $MACJAIL not found!"; exit 1; }
 JAILHOME=$MACJAIL$HOME
 JAILSOCK=$MACJAIL/${SSH_AUTH_SOCK%/*}
 
@@ -12,28 +12,28 @@ mycp() {
   BN=${FROM##*/}
   DN=${FROM%/*}
   TO=$MACJAIL/$DN
-  test -r $TO/$BN && return 0
-  echo $FROM
+  test -r "$TO/$BN" && return 0
+  echo "$FROM"
   mkdir -p "${TO}"
   cp "$FROM" "$TO"
 }
 
 mymkdir() {
-  test -d $1 || mkdir -pv $1
+  test -d "$1" || mkdir -pv "$1"
 }
 
-mymkdir $JAILHOME
-mymkdir $JAILSOCK
-mymkdir $MACJAIL/var/run
+mymkdir "$JAILHOME"
+mymkdir "$JAILSOCK"
+mymkdir "$MACJAIL"/var/run
 mycp /etc/resolv.conf
 mycp /etc/hosts
-ln -f /var/run/mDNSResponder $MACJAIL/var/run/
-ln -f $SSH_AUTH_SOCK $JAILSOCK
+ln -f /var/run/mDNSResponder "$MACJAIL"/var/run/
+ln -f "$SSH_AUTH_SOCK" "$JAILSOCK"
 env -i \
-  TERM=$TERM \
+  TERM="$TERM" \
   PS1="${MACJAIL##*/}\$ " \
-  SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
-  SHELL=$SHELL \
-  HOME=$HOME \
-  PATH=$PATH \
-  /usr/sbin/chroot -u $MYUSER $MACJAIL $SHELL
+  SSH_AUTH_SOCK="$SSH_AUTH_SOCK" \
+  SHELL="$SHELL" \
+  HOME="$HOME" \
+  PATH="$PATH" \
+  /usr/sbin/chroot -u "$MYUSER" "$MACJAIL" "$SHELL"
